a5d31a3d951c14c41c5384094db186a31295832a
Linh Vo <lvo@umbel.com>
2016-09-14 11:21:19 -0500

    verstion 0.2.2.4, update pilosa settings

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pilosa-driver',
-    version='0.2.2.3',
-    description='Pilosa support query string',
+    version='0.2.2.4',
+    description='Update pilosa settings',
     #long_description=long_description,
 
     # The project's main homepage.
