8ba44974a886c959652ddc14b0d454e24376ed30
Cody Soyland <cody@soyland.org>
2017-02-23 09:04:14 -0600

    No support for Python 2.6

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
